7313d844f64da28f61ef577865f9a0c05ff2cbc4
Anushervon Khidirov <ankhid98@gmail.com>
2025-03-21 20:42:49 +0500

    feat: create user method added

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,11 +1,16 @@
 import type { User } from './user.type';
 import type { ValueErrorReturnPromise } from '../common/type/value-error-return.type';
 
+import { ResultSetHeader } from 'mysql2';
 import { db } from '../database/database';
-import { NotFoundException } from '../exception/exception';
+import { CreateUserDto } from './dto/create-user.dto';
+import { NotFoundException, InternalServerErrorException } from '../exception/exception';
+import { QueryGenerator } from '../database/query.helper';
+import { ConflictException } from '../exception/conflict.exception';
 
 export class UserService {
   private readonly table = 'users';
+  private readonly queryGenerator = new QueryGenerator<User>(this.table);
 
   constructor() {
     this.createTable();
@@ -19,18 +24,49 @@ export class UserService {
     );`);
   }
 
-  async findOne(id: number): ValueErrorReturnPromise<User> {
-    const [result] = await db.query(`SELECT * FROM ${this.table} WHERE id = ?`, [id]);
-    const users = <User[]>result;
+  async findOne(where: Partial<User>): ValueErrorReturnPromise<User> {
+    try {
+      const [result] = await db.query(...this.queryGenerator.select(where));
+      const users = <User[]>result;
 
-    if (!users.length) return [null, new NotFoundException('User not found')];
-    return [users[0], null];
+      if (!users.length) return [null, new NotFoundException('User not found')];
+      return [users[0], null];
+    } catch (_) {
+      return [null, new InternalServerErrorException()];
+    }
   }
 
   async findAll(): ValueErrorReturnPromise<User[]> {
-    const [result] = await db.query(`SELECT * FROM ${this.table}`);
-    const users = <User[]>result;
+    try {
+      const [result] = await db.query(...this.queryGenerator.select());
+      const users = <User[]>result;
 
-    return [users, null];
+      return [users, null];
+    } catch (err) {
+      return [null, new InternalServerErrorException()];
+    }
+  }
+
+  async create(createUserDto: CreateUserDto): ValueErrorReturnPromise<User> {
+    try {
+      const [isExist] = await this.findOne({ email: createUserDto.email });
+      if (isExist) {
+        return [
+          null,
+          new ConflictException(`User with email '${createUserDto.email}' already exists`),
+        ];
+      }
+
+      const [result] = await db.query<ResultSetHeader>(
+        ...this.queryGenerator.insert(createUserDto),
+      );
+
+      const [user, err] = await this.findOne({ id: result.insertId });
+
+      if (err) return [null, err];
+      return [user, null];
+    } catch (_) {
+      return [null, new InternalServerErrorException()];
+    }
   }
 }
