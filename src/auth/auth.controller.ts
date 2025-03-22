2f7775b5043bd6407b4b7e20f5f0b3f1e9bd98ec
Anushervon Khidirov <ankhid98@gmail.com>
2025-03-22 21:49:09 +0500

    feat: auth controller added

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,9 +1,53 @@
-import { Request, Response } from 'express';
+import type { Request, Response } from 'express';
+import { AuthService } from './auth.service';
+import { CreateUserDto, CreateUserDtoValidation } from '../user/dto/create-user.dto';
+import { BadRequestException } from '../exception/bad-request.exception';
+import { SignInDto, SignInDtoValidation } from './dto/sign-in.dto';
 
 export class AuthController {
-  async SignIn(req: Request, res: Response) {}
+  service: AuthService;
 
-  async SignUp(req: Request, res: Response) {}
+  constructor() {
+    this.service = new AuthService();
+  }
 
-  async SignOut(req: Request, res: Response) {}
+  async SignIn(req: Request, res: Response) {
+    const signInDto = new SignInDtoValidation(req.body).validate();
+
+    if (!(signInDto instanceof SignInDto)) {
+      const exception = new BadRequestException(signInDto);
+      res.status(exception.statusCode).send(exception);
+      return;
+    }
+
+    const [token, err] = await this.service.SignIn(signInDto);
+
+    if (err) {
+      res.status(err.statusCode).send(err);
+      return;
+    }
+
+    res.status(200).send(token);
+  }
+
+  async SignUp(req: Request, res: Response) {
+    const createUserDto = new CreateUserDtoValidation(req.body).validate();
+
+    if (!(createUserDto instanceof CreateUserDto)) {
+      const exception = new BadRequestException(createUserDto);
+      res.status(exception.statusCode).send(exception);
+      return;
+    }
+
+    const [token, err] = await this.service.SignUp(createUserDto);
+
+    if (err) {
+      res.status(err.statusCode).send(err);
+      return;
+    }
+
+    res.status(200).send(token);
+  }
+
+  async SignOut(_: Request, res: Response) {}
 }
