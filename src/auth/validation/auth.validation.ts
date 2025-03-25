c6027a8f309ff89565556b58db6415c59909669c
Anushervon Khidirov <ankhid98@gmail.com>
2025-03-25 19:39:35 +0500

    feat: file renamed and sign out validation added

--- src/auth/validation/auth.validation.ts
+++ src/auth/validation/auth.validation.ts
@@ -4,3 +4,7 @@ export const SignInValidation = Joi.object({
   email: Joi.string().email().required(),
   password: Joi.string().required(),
 });
+
+export const SignOutValidation = Joi.object({
+  refreshToken: Joi.string().required()
+})
