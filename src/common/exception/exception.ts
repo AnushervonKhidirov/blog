efca6ce69930ed283f52d6ea6286c33a10571851
Anushervon Khidirov <ankhid98@gmail.com>
2025-03-26 10:56:14 +0500

    feat: unauthorized exception added

--- src/common/exception/exception.ts
+++ src/common/exception/exception.ts
@@ -3,6 +3,7 @@ import { NotFoundException } from './not-found.exception';
 import { BadRequestException } from './bad-request.exception';
 import { InternalServerErrorException } from './internal-server-error.exception';
 import { ConflictException } from './conflict.exception';
+import { UnauthorizedException } from './unauthorized.exception';
 
 export {
   HttpException,
@@ -10,4 +11,5 @@ export {
   BadRequestException,
   InternalServerErrorException,
   ConflictException,
+  UnauthorizedException,
 };
