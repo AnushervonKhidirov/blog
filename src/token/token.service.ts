73b8a24ea787c0e7649c7ceaaad11f2f3da1c1e4
Anushervon Khidirov <ankhid98@gmail.com>
2025-03-26 10:58:20 +0500

    feat: token verify method added

--- src/token/token.service.ts
+++ src/token/token.service.ts
@@ -1,12 +1,13 @@
 import type { JwtPayload } from 'jsonwebtoken';
 import type { ReturnWithErr, ReturnPromiseWithErr } from '@type/return-with-err.type';
-import type { Token } from './token.type';
 import type { Token as UserToken } from '@prisma/client';
+import type { Token, DecodedToken } from './token.type';
 
 import { PrismaClient } from '@prisma/client';
-import { sign } from 'jsonwebtoken';
+import { sign, verify } from 'jsonwebtoken';
 import { logger } from '@config/logger/logger';
-import { InternalServerErrorException } from '@exception';
+import { InternalServerErrorException, NotFoundException, UnauthorizedException } from '@exception';
+import { SignOutDto } from '../auth/dto/sign-out.dto';
 
 export class TokenService {
   private readonly repository = new PrismaClient().token;
@@ -38,9 +39,49 @@ export class TokenService {
       if (!userToken) return [null, new InternalServerErrorException('Unable to save token')];
       return [userToken, null];
     } catch (err) {
-      console.log(err);
       logger.error(err);
       return [null, new InternalServerErrorException()];
+    }
+  }
+
+  async delete({ refreshToken }: SignOutDto): ReturnPromiseWithErr<UserToken> {
+    try {
+      const token = await this.repository.delete({ where: { refreshToken } });
+      if (!token) return [null, new NotFoundException('Token not found')];
+      return [token, null];
+    } catch (err) {
+      logger.error(err);
+      return [null, new InternalServerErrorException()];
+    }
+  }
+
+  verify({ accessToken, refreshToken }: Partial<Token>): ReturnWithErr<DecodedToken> {
+    const decoded: DecodedToken = {
+      accessToken: null,
+      refreshToken: null,
+    };
+
+    try {
+      if (!this.accessSecret || !this.refreshSecret) {
+        throw new Error("Can't get access or refresh secret key");
+      }
+
+      if (!accessToken && !refreshToken) {
+        throw new Error('Token was not provided');
+      }
+
+      if (accessToken) {
+        decoded.accessToken = verify(accessToken, this.accessSecret);
+      }
+
+      if (refreshToken) {
+        decoded.refreshToken = verify(refreshToken, this.refreshSecret);
+      }
+
+      return [decoded, null];
+    } catch (err) {
+      logger.error(err);
+      return [null, new UnauthorizedException()];
     }
   }
 }
