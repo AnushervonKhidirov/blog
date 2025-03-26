4b05d5cdffa71f8506fe3a4683e0ecfa0271d01a
Anushervon Khidirov <ankhid98@gmail.com>
2025-03-26 20:18:11 +0500

    refactoring: controller errors handling refactored

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -2,6 +2,7 @@ import type { Request, Response } from 'express';
 
 import { validate } from '@validation/validation';
 import { paramId, paramUserId } from '@validation/schema/param.validation';
+import responseError from '@helper/response-error.helper';
 import { CreatePostValidation, UpdatePostValidation } from './validation/user.validation';
 import { CreatePostDto } from './dto/create-post.dto';
 import { UpdatePostDto } from './dto/update-post.dto';
@@ -12,112 +13,94 @@ export class PostController {
   service = new PostService();
 
   async findOne(req: Request, res: Response) {
-    const [params, validationErr] = validate<{ id: number }>(paramId, req.params);
-    if (validationErr) {
-      res.status(validationErr.statusCode).send(validationErr);
-      return;
-    }
+    try {
+      const [params, validationErr] = validate<{ id: number }>(paramId, req.params);
+      if (validationErr) throw validationErr;
 
-    const [post, err] = await this.service.findOne({ id: params.id }, true);
-    if (err) {
-      res.status(err.statusCode).send(err);
-      return;
-    }
+      const [post, err] = await this.service.findOne({ id: params.id }, true);
+      if (err) throw err;
 
-    res.status(200).send(post);
+      res.status(200).send(post);
+    } catch (err) {
+      responseError(err, res);
+    }
   }
 
   async findMany(req: Request, res: Response) {
-    const [params, validationErr] = validate<{ userId: number }>(paramUserId, req.query);
-    if (validationErr) {
-      res.status(validationErr.statusCode).send(validationErr);
-      return;
-    }
+    try {
+      const [params, validationErr] = validate<{ userId: number }>(paramUserId, req.query);
+      if (validationErr) throw validationErr;
 
-    const [posts, err] = await this.service.findMany(params, true);
-    if (err) {
-      res.status(err.statusCode).send(err);
-      return;
-    }
+      const [posts, err] = await this.service.findMany(params, true);
+      if (err) throw err;
 
-    res.status(200).send(posts);
+      res.status(200).send(posts);
+    } catch (err) {
+      responseError(err, res);
+    }
   }
 
   async create(req: Request, res: Response) {
-    if (!req['userId']) {
-      const exception = new UnauthorizedException();
-      res.status(exception.statusCode).send(exception);
-      return;
+    try {
+      if (!req['userId']) throw new UnauthorizedException();
+
+      const userId = parseInt(req['userId']);
+
+      const [createPostDto, validationErr] = validate<CreatePostDto>(
+        CreatePostValidation,
+        req.body,
+      );
+
+      if (validationErr) throw validationErr;
+
+      const [post, err] = await this.service.create(userId, createPostDto);
+      if (err) throw err;
+
+      res.status(200).send(post);
+    } catch (err) {
+      responseError(err, res);
     }
-
-    const userId = parseInt(req['userId']);
-
-    const [createPostDto, validationErr] = validate<CreatePostDto>(CreatePostValidation, req.body);
-    if (validationErr) {
-      res.status(validationErr.statusCode).send(validationErr);
-      return;
-    }
-
-    const [post, err] = await this.service.create(userId, createPostDto);
-    if (err) {
-      res.status(err.statusCode).send(err);
-      return;
-    }
-
-    res.status(200).send(post);
   }
 
   async update(req: Request, res: Response) {
-    if (!req['userId']) {
-      const exception = new UnauthorizedException();
-      res.status(exception.statusCode).send(exception);
-      return;
+    try {
+      if (!req['userId']) throw new UnauthorizedException();
+
+      const userId = parseInt(req['userId']);
+
+      const [params, paramsErr] = validate<{ id: number }>(paramId, req.params);
+      if (paramsErr) throw paramsErr;
+
+      const [updatePostDto, validationErr] = validate<UpdatePostDto>(
+        UpdatePostValidation,
+        req.body,
+      );
+      if (validationErr) throw validationErr;
+
+      const [post, err] = await this.service.update(userId, params.id, updatePostDto);
+      if (err) throw err;
+
+      res.status(200).send(post);
+    } catch (err) {
+      responseError(err, res);
     }
-
-    const userId = parseInt(req['userId']);
-
-    const [params, paramsErr] = validate<{ id: number }>(paramId, req.params);
-    if (paramsErr) {
-      res.status(paramsErr.statusCode).send(paramsErr);
-      return;
-    }
-
-    const [updatePostDto, validationErr] = validate<UpdatePostDto>(UpdatePostValidation, req.body);
-    if (validationErr) {
-      res.status(validationErr.statusCode).send(validationErr);
-      return;
-    }
-
-    const [post, err] = await this.service.update(userId, params.id, updatePostDto);
-    if (err) {
-      res.status(err.statusCode).send(err);
-      return;
-    }
-
-    res.status(200).send(post);
   }
 
   async delete(req: Request, res: Response) {
-    if (!req['userId']) {
-      const exception = new UnauthorizedException();
-      res.status(exception.statusCode).send(exception);
-      return;
+    try {
+      if (!req['userId']) throw new UnauthorizedException();
+
+      const userId = parseInt(req['userId']);
+
+      const [params, paramsErr] = validate<{ id: number }>(paramId, req.params);
+      if (paramsErr) throw paramsErr;
+
+      const [post, err] = await this.service.delete(userId, params.id);
+      if (err) throw err;
+
+      res.status(200).send(post);
+    } catch (err) {
+      responseError(err, res);
     }
-
-    const userId = parseInt(req['userId']);
-
-    const [params, paramsErr] = validate<{ id: number }>(paramId, req.params);
-    if (paramsErr) {
-      res.status(paramsErr.statusCode).send(paramsErr);
-      return;
-    }
-
-    const [post, err] = await this.service.delete(userId, params.id);
-    if (err) {
-      res.status(err.statusCode).send(err);
-      return;
-    }
-
-    res.status(200).send(post);
   }
 }
