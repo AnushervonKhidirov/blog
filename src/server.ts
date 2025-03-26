1f4f1da087d037a72aed71a5ed5beb12eb49f9e7
Anushervon Khidirov <ankhid98@gmail.com>
2025-03-26 16:29:32 +0500

    feat: post router added

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,9 @@ import express from 'express';
 import cors from 'cors';
 
 import { AuthMiddleware } from './middleware/auth.middleware';
-import { UserRouter } from './user/user.router';
 import { AuthRouter } from './auth/auth.router';
+import { UserRouter } from './user/user.router';
+import { PostRouter } from './post/post.router';
 
 const app = express();
 
@@ -11,7 +12,8 @@ app.use(express.json());
 app.use(cors());
 app.use(AuthMiddleware());
 
-app.use('/users', UserRouter);
 app.use('/auth', AuthRouter);
+app.use('/users', UserRouter);
+app.use('/posts', PostRouter);
 
 app.listen(4000);
