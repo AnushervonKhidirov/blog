eee4d7ae4c857bc7c2de4059b6a19243fb386247
Anushervon Khidirov <ankhid98@gmail.com>
2025-03-26 16:30:47 +0500

    feat: post controller added

---
+++ src/post/post.controller.ts
@@ -0,0 +1,101 @@
+import type { Request, Response } from 'express';
+
+import { validate } from '@validation/validation';
+import { paramId } from '@validation/schema/param.validation';
+import { CreatePostValidation, UpdatePostValidation } from './validation/user.validation';
+import { CreatePostDto } from './dto/create-post.dto';
+import { UpdatePostDto } from './dto/update-post.dto';
+import { UnauthorizedException } from '@exception';
+import { PostService } from './post.service';
+
+export class PostController {
+  service = new PostService();
+
+  async findOne(req: Request, res: Response) {
+    const [params, validationErr] = validate<{ id: number }>(paramId, req.params);
+    if (validationErr) {
+      res.status(validationErr.statusCode).send(validationErr);
+      return;
+    }
+
+    const [post, err] = await this.service.findOne({ id: params.id }, true);
+    if (err) {
+      res.status(err.statusCode).send(err);
+      return;
+    }
+
+    res.status(200).send(post);
+  }
+
+  async findMany(req: Request, res: Response) {
+    const [posts, err] = await this.service.findMany(undefined, true);
+    if (err) {
+      res.status(err.statusCode).send(err);
+      return;
+    }
+
+    res.status(200).send(posts);
+  }
+
+  async create(req: Request, res: Response) {
+    if (!req['userId']) {
+      const exception = new UnauthorizedException();
+      res.status(exception.statusCode).send(exception);
+      return;
+    }
+
+    const userId = parseInt(req['userId']);
+
+    const [createPostDto, validationErr] = validate<CreatePostDto>(CreatePostValidation, req.body);
+    if (validationErr) {
+      res.status(validationErr.statusCode).send(validationErr);
+      return;
+    }
+
+    const [post, err] = await this.service.create(userId, createPostDto);
+    if (err) {
+      res.status(err.statusCode).send(err);
+      return;
+    }
+
+    res.status(200).send(post);
+  }
+
+  async update(req: Request, res: Response) {
+    const [params, paramsErr] = validate<{ id: number }>(paramId, req.params);
+    if (paramsErr) {
+      res.status(paramsErr.statusCode).send(paramsErr);
+      return;
+    }
+
+    const [updatePostDto, validationErr] = validate<UpdatePostDto>(UpdatePostValidation, req.body);
+    if (validationErr) {
+      res.status(validationErr.statusCode).send(validationErr);
+      return;
+    }
+
+    const [post, err] = await this.service.update(params.id, updatePostDto);
+    if (err) {
+      res.status(err.statusCode).send(err);
+      return;
+    }
+
+    res.status(200).send(post);
+  }
+
+  async delete(req: Request, res: Response) {
+    const [params, paramsErr] = validate<{ id: number }>(paramId, req.params);
+    if (paramsErr) {
+      res.status(paramsErr.statusCode).send(paramsErr);
+      return;
+    }
+
+    const [post, err] = await this.service.delete(params.id);
+    if (err) {
+      res.status(err.statusCode).send(err);
+      return;
+    }
+
+    res.status(200).send(post);
+  }
+}
